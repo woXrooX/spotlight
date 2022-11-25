3d92e58a10e7381842d24f6f899666a9561b811d
woXrooX <woXrooX@gmail.com>
2022-11-25 10:50:30 +0100

    Making Life Easier + Colors Corresponding To The Output Types + More Related Names

--- source/spotlight.hpp
+++ source/spotlight.hpp
@@ -3,38 +3,40 @@
 
 #include <iostream>
 #include <sys/socket.h>
+#include <netinet/in.h> // IPPROTO_TCP
+// #include <netinet/tcp.h>
+// #include <sys/types.h>
 
 namespace woXrooX{
   class Spotlight final{
   public:
     static void observe(){
-      std::cout << "Starting..." << '\n';
-      Spotlight::create_raw_socket();
-      Spotlight::in();
+      std::cout << "\033[1;33mStarting...\033[0m" << '\n';
+      Spotlight::create_socket();
+      // Spotlight::in();
 
     }
 
   private:
-    static void create_raw_socket(){
-      Spotlight::raw_socket_fd = socket(PF_PACKET, SOCK_RAW, 6);
-      // Spotlight::raw_socket_fd = socket(AF_INET, SOCK_RAW, 6);
-      if(Spotlight::raw_socket_fd == -1) std::cout << "Failed To Create Socket Descriptor." << '\n';
-      else std::cout << "Socket Descriptor Created Successfully." << '\n';
+    static void create_socket(){
+      Spotlight::fd_socket = socket(AF_PACKET, SOCK_RAW, IPPROTO_TCP);
+      if(Spotlight::fd_socket == -1) std::cout << "\033[1;31mFailed To Create Socket Descriptor.\033[0m" << '\n';
+      else std::cout << "\033[0;32mSocket Descriptor Created Successfully.\033[0m" << '\n';
 
     }
 
     static void in(){
-      if(Spotlight::raw_socket_fd == -1) return;
+      if(Spotlight::fd_socket == -1) return;
 
-      char inData[Spotlight::BUFFER];
+      char inData[Spotlight::BUFFER_SIZE];
       int received_data_size = 0;
 
       do{
-        Spotlight::bytes_received = recvfrom(Spotlight::raw_socket_fd, inData, Spotlight::BUFFER, 0, NULL, NULL);
+        Spotlight::bytes_received = recvfrom(Spotlight::fd_socket, inData, Spotlight::BUFFER_SIZE, 0, NULL, NULL);
 
         received_data_size += Spotlight::bytes_received;
 
-        if(received_data_size > Spotlight::BUFFER-1 || inData[Spotlight::BUFFER-1] == '\n') break;
+        if(received_data_size > Spotlight::BUFFER_SIZE-1 || inData[Spotlight::BUFFER_SIZE-1] == '\n') break;
         if(Spotlight::bytes_received == -1){std::cout << "Failed To Receive Data." << '\n'; return;}
         if(Spotlight::bytes_received == 0){std::cout << "End Of The Line" << '\n'; break;}
         if(Spotlight::bytes_received > 0){
@@ -48,14 +50,14 @@ namespace woXrooX{
     }
 
     // Variables
-    static int raw_socket_fd;
+    static int fd_socket;
     static int bytes_received;
-    static int BUFFER;
+    static int BUFFER_SIZE;
 
   };
-  int Spotlight::raw_socket_fd = -1;
+  int Spotlight::fd_socket = -1;
   int Spotlight::bytes_received = 0;
-  // int Spotlight::BUFFER = 512;
-  int Spotlight::BUFFER = 262144; // 512kb | 256kb
+  // int Spotlight::BUFFER_SIZE = 512;
+  int Spotlight::BUFFER_SIZE = 262144; // 512kb | 256kb
 }
 #endif
