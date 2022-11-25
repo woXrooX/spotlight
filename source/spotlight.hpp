d7fae6c87d199f9c5f5110253ff935e240293961
woXrooX <woXrooX@gmail.com>
2022-11-25 21:45:48 +0100

    Var Name Fix

--- source/spotlight.hpp
+++ source/spotlight.hpp
@@ -5,8 +5,8 @@
 #include <netinet/in.h>       // socket & AF_PACKET & SOCK_RAW & IPPROTO_TCP
 #include <unistd.h>           // read
 #include <net/ethernet.h>     // ETH_P_ALL
-// #include <netinet/ip.h>       // struct iphdr -> Unix-Like
-#include <linux/ip.h>         // struct iphdr -> Linux
+#include <netinet/ip.h>       // struct iphdr -> Unix-Like
+// #include <linux/ip.h>         // struct iphdr -> Linux
 
 // #include <linux/if_packet.h>
 // #include <arpa/inet.h>
@@ -34,6 +34,7 @@ namespace woXrooX{
       // IPPROTO_TCP
       // All Protocols -> htons(ETH_P_ALL)
       Spotlight::fd_socket = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+      // Spotlight::fd_socket = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
       if(Spotlight::fd_socket < 0) std::cout << "\033[40;31m" << "Failed To Create Socket Descriptor." << "\033[0m\n";
       else std::cout << "\033[1;32m" << "Socket Descriptor Created Successfully." << "\033[0m\n";
 
@@ -54,7 +55,7 @@ namespace woXrooX{
         if(Spotlight::bytes_received == -1){std::cout << "\033[40;31m" << "Failed To Receive Data." << "\033[0m\n"; return;}
         if(Spotlight::bytes_received == 0){std::cout << "\033[40;33m" << "End Of The Line" << "\033[0m\n"; break;}
         if(Spotlight::bytes_received > 0){
-          std::cout << "\033[30;94m" << "Data Sniffed Successfully!" << "\033[0m\n";
+          // std::cout << "\033[30;94m" << "Data Sniffed Successfully!" << "\033[0m\n";
           // std::cout << Spotlight::bytes_received << '\n';
           // std::cout << inData << '\n';
           Spotlight::processPacket(inData, received_data_size);
@@ -100,12 +101,14 @@ namespace woXrooX{
       }
 
       std::cout
+      << "\x1b[2J"    // Clear Entire Terminal
+      << "\x1b[1;1f"  // Move Cursor To 1:1
       << "ICMP: "       << Spotlight::ICMP
-      << "\tIGMP: "     << Spotlight::IGMP
-      << "\tTCP: "      << Spotlight::ICMP
-      << "\tUDP: "      << Spotlight::ICMP
-      << "\tTotal: "    << Spotlight::total
-      << "\tUnknown: "  << Spotlight::unknown
+      << "\nIGMP: "     << Spotlight::IGMP
+      << "\nTCP: "      << Spotlight::TCP
+      << "\nUDP: "      << Spotlight::UDP
+      << "\nTotal: "    << Spotlight::total
+      << "\nUnknown: "  << Spotlight::unknown
       << '\n';
 
     }
@@ -134,8 +137,8 @@ namespace woXrooX{
 
   int Spotlight::fd_socket = -1;
   int Spotlight::bytes_received = 0;
-  int Spotlight::BUFFER_SIZE = 512;
-  // int Spotlight::BUFFER_SIZE = 262144; // 512kb | 256kb
+  // int Spotlight::BUFFER_SIZE = 512;
+  int Spotlight::BUFFER_SIZE = 262144; // 512kb | 256kb
 
 }
 #endif
