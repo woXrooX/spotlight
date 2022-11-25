4058ce4738e93b273c08c04765f239a93f1a29bf
woXrooX <woXrooX@gmail.com>
2022-11-25 15:13:42 +0100

    Processing all data but can not extract ip header infos yet

--- source/spotlight.hpp
+++ source/spotlight.hpp
@@ -5,6 +5,8 @@
 #include <netinet/in.h>       // socket & AF_PACKET & SOCK_RAW & IPPROTO_TCP
 #include <unistd.h>           // read
 #include <net/ethernet.h>     // ETH_P_ALL
+// #include <netinet/ip.h>       // struct iphdr -> Unix-Like
+#include <linux/ip.h>         // struct iphdr -> Linux
 
 // #include <linux/if_packet.h>
 // #include <arpa/inet.h>
@@ -52,8 +54,10 @@ namespace woXrooX{
         if(Spotlight::bytes_received == -1){std::cout << "\033[40;31m" << "Failed To Receive Data." << "\033[0m\n"; return;}
         if(Spotlight::bytes_received == 0){std::cout << "\033[40;33m" << "End Of The Line" << "\033[0m\n"; break;}
         if(Spotlight::bytes_received > 0){
-          std::cout << "\033[40;94m" << "Data Received Successfully!" << "\033[0m\n";
-          std::cout << inData << '\n';
+          std::cout << "\033[30;94m" << "Data Sniffed Successfully!" << "\033[0m\n";
+          // std::cout << Spotlight::bytes_received << '\n';
+          // std::cout << inData << '\n';
+          Spotlight::processPacket(inData, received_data_size);
           break;
         }
 
@@ -61,15 +65,77 @@ namespace woXrooX{
 
     }
 
-    // Variables
+    // void ProcessPacket(unsigned char* buffer, int size){
+    static void processPacket(unsigned char* inData, int size){
+
+      // https://stackoverflow.com/questions/42840636/difference-between-struct-ip-and-struct-iphdr
+      // IP Header
+      struct iphdr *iph = (struct iphdr*)inData;
+
+      // Total Connections
+      Spotlight::total++;
+
+      // Individual Connections / Protocols
+      switch(iph->protocol){
+        case 1: // ICMP
+          Spotlight::ICMP++;
+          break;
+
+        case 2: // IGMP
+          Spotlight::IGMP++;
+          break;
+
+        case 6: // TCP
+          Spotlight::TCP++;
+          break;
+
+        case 17: // UDP
+          Spotlight::UDP++;
+          break;
+
+        default:
+          Spotlight::unknown++;
+          break;
+
+      }
+
+      std::cout
+      << "ICMP: "       << Spotlight::ICMP
+      << "\tIGMP: "     << Spotlight::IGMP
+      << "\tTCP: "      << Spotlight::ICMP
+      << "\tUDP: "      << Spotlight::ICMP
+      << "\tTotal: "    << Spotlight::total
+      << "\tUnknown: "  << Spotlight::unknown
+      << '\n';
+
+    }
+
+    static void printIpHeader(){
+
+    }
+
+    ///////// Variables
+    // Counts
+    static int total, unknown;
+    static int ICMP, IGMP, TCP, UDP;
+
     static int fd_socket;
     static int bytes_received;
     static int BUFFER_SIZE;
 
   };
+
+  int Spotlight::total = 0;
+  int Spotlight::unknown = 0;
+  int Spotlight::ICMP = 0;
+  int Spotlight::IGMP = 0;
+  int Spotlight::TCP = 0;
+  int Spotlight::UDP = 0;
+
   int Spotlight::fd_socket = -1;
   int Spotlight::bytes_received = 0;
-  // int Spotlight::BUFFER_SIZE = 512;
-  int Spotlight::BUFFER_SIZE = 262144; // 512kb | 256kb
+  int Spotlight::BUFFER_SIZE = 512;
+  // int Spotlight::BUFFER_SIZE = 262144; // 512kb | 256kb
+
 }
 #endif
