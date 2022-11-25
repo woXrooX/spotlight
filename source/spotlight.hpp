9c4d0674a5f096b86f57e8635c7d31937c3d60b8
woXrooX <woXrooX@gmail.com>
2022-11-25 11:38:34 +0100

    Started Receiving Data From All Kinds Of Protocols

--- source/spotlight.hpp
+++ source/spotlight.hpp
@@ -2,8 +2,13 @@
 #define SPOTLIGHT_H
 
 #include <iostream>
-#include <sys/socket.h>
-#include <netinet/in.h> // IPPROTO_TCP
+#include <netinet/in.h>       // socket & AF_PACKET & SOCK_RAW & IPPROTO_TCP
+#include <unistd.h>           // read
+#include <net/ethernet.h>     // ETH_P_ALL
+
+// #include <linux/if_packet.h>
+// #include <arpa/inet.h>
+// #include <sys/socket.h>
 // #include <netinet/tcp.h>
 // #include <sys/types.h>
 
@@ -13,13 +18,20 @@ namespace woXrooX{
     static void observe(){
       std::cout << "\033[1;33mStarting...\033[0m" << '\n';
       Spotlight::create_socket();
-      // Spotlight::in();
+
+      while(true){
+        Spotlight::in();
+
+      }
 
     }
 
   private:
     static void create_socket(){
-      Spotlight::fd_socket = socket(AF_PACKET, SOCK_RAW, IPPROTO_TCP);
+      // 0 indicates that the caller does not want to specify the protocol and will leave it up to the service provider.
+      // IPPROTO_TCP
+      // All Protocols -> htons(ETH_P_ALL)
+      Spotlight::fd_socket = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
       if(Spotlight::fd_socket == -1) std::cout << "\033[1;31mFailed To Create Socket Descriptor.\033[0m" << '\n';
       else std::cout << "\033[0;32mSocket Descriptor Created Successfully.\033[0m" << '\n';
 
@@ -28,21 +40,21 @@ namespace woXrooX{
     static void in(){
       if(Spotlight::fd_socket == -1) return;
 
-      char inData[Spotlight::BUFFER_SIZE];
+      unsigned char inData[Spotlight::BUFFER_SIZE];
       int received_data_size = 0;
 
       do{
-        Spotlight::bytes_received = recvfrom(Spotlight::fd_socket, inData, Spotlight::BUFFER_SIZE, 0, NULL, NULL);
+        Spotlight::bytes_received = read(Spotlight::fd_socket, inData, Spotlight::BUFFER_SIZE);
 
         received_data_size += Spotlight::bytes_received;
 
-        if(received_data_size > Spotlight::BUFFER_SIZE-1 || inData[Spotlight::BUFFER_SIZE-1] == '\n') break;
+        // if(received_data_size > Spotlight::BUFFER_SIZE-1 || inData[Spotlight::BUFFER_SIZE-1] == '\n') break;
         if(Spotlight::bytes_received == -1){std::cout << "Failed To Receive Data." << '\n'; return;}
         if(Spotlight::bytes_received == 0){std::cout << "End Of The Line" << '\n'; break;}
         if(Spotlight::bytes_received > 0){
           std::cout <<  "Data Received Successfully" << '\n';
           std::cout << inData << '\n';
-          // break;
+          break;
         }
 
       }while(true);
