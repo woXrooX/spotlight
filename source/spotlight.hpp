f06cdaf2fbe25349bbe6633092e54cf1f58e88f0
woXrooX <woXrooX@gmail.com>
2023-03-12 18:15:23 +0500

    + tcp header

--- source/spotlight.hpp
+++ source/spotlight.hpp
@@ -11,11 +11,11 @@
 
 #include <net/ethernet.h>     // ETH_P_ALL
 #include <netinet/ip.h>       // struct iphdr -> Unix-Like
+#include <netinet/tcp.h>      // struct tcphdr
 // #include <linux/ip.h>         // struct iphdr -> Linux
 
 // #include <linux/if_packet.h>
 // #include <sys/socket.h>
-// #include <netinet/tcp.h>
 // #include <sys/types.h>
 
 namespace woXrooX{
@@ -70,27 +70,78 @@ namespace woXrooX{
 
     ////// Processes
     static void processPacket(unsigned char* inData, int size){
+      std::cout << "\n---------------------- processPacket ----------------------" << '\n';
+
       // If you get 0x800 (ETH_P_IP), it means that the next header is the IP header. Later, we will consider the next header as the IP header
 
-      // Ethernet Header
+      //// Ethernet Header
+      // In the raw data, the Ethernet header comes before the IP header. The Ethernet header is typically 14 bytes long, and the IP header follows it.
       struct ethhdr *eth = (struct ethhdr*)inData;
+      // Destination MAC Address h_dest
+      // Source MAC Address h_source
+      // Protocol Type h_proto
+
+      //// VLAN Tag
+      // struct vlan_tag *vlan = (struct vlan_tag*)(inData + sizeof(struct ethhdr));
+      // if(eth->h_proto == htons(ETH_P_8021Q)){
+      //   std::cout << "VLAN ID: " << ntohs(vlan->vlan_id) << std::endl;
+      // }
+
+      struct iphdr *iph = (struct iphdr*)(inData + sizeof(struct ethhdr));
 
       // Total Connections
       Spotlight::total++;
 
+      std::cout << "Ethernet Header Protocol In HEX: 0x" << std::setfill('0') << std::setw(4) << std::hex << ntohs(eth->h_proto) << '\n';
+      std::cout << "Ethernet Header Protocol In DEC: " << eth->h_proto << '\n';
+
       // Individual Connections / Protocols
-      switch(eth->h_proto){
-        case 8: // EGP
+      switch(ntohs(eth->h_proto)){
+      // switch(eth->h_proto){
+        case 0x08: // EGP
+          std::cout << "EGP" << '\n';
           Spotlight::EGP++;
+          // Spotlight::outETH(eth->h_proto, eth->h_source, eth->h_dest);
           break;
 
+
+        case 0x0800: // Next Is IP Header | IPv4
+          std::cout << "IP Header" << '\n';
+          // Spotlight::outIPH(iph);
+          break;
+
+        case 0x0804: // HDLC
+          std::cout << "HDLC" << '\n';
+          break;
+
+        case 0x0805: // STP (Spanning Tree Protocol)
+          std::cout << "STP" << '\n';
+          break;
+
+
+        case 0x0806: // Address Resolution Protocol (ARP)
+          std::cout << "ARP" << '\n';
+          break;
+
+
+        case 0x8100: // VLAN Tag
+          std::cout << "Next Header Will Be VLAN Tag" << '\n';
+          break;
+
+
+        case 0x86dd: // IPv6
+          std::cout << "IPv6" << '\n';
+          break;
+
+
         default:
+          std::cout << "Unknown" << '\n';
           Spotlight::unknown++;
           break;
 
       }
 
-      // Spotlight::outETH(eth->h_proto, eth->h_source, eth->h_dest);
+      Spotlight::outETH(eth->h_proto, eth->h_source, eth->h_dest);
       Spotlight::processIPH(inData);
       // Spotlight::outCounters();
 
@@ -99,6 +150,8 @@ namespace woXrooX{
     static void processIPH(unsigned char* inData){
       // IP Header
       struct iphdr *iph = (struct iphdr*)(inData + sizeof(struct ethhdr));
+
+      Spotlight::outIPH(iph);
 
       switch(iph->protocol){
         case 1: // ICMP
@@ -111,6 +164,8 @@ namespace woXrooX{
 
         case 6: // TCP
           Spotlight::TCP++;
+          Spotlight::outTCPH(inData);
+
           break;
 
         case 8: // EGP
@@ -127,15 +182,13 @@ namespace woXrooX{
 
       }
 
-      Spotlight::outIPH(iph);
-
-      delete iph;
+      // delete iph;
 
     }
 
     ////// Outs
     static void outETH(int protocol, unsigned char source[6], unsigned char destination[6]){
-      std::cout << "Ethernet Header" << '\n';
+      std::cout << "\n---------------------- outETH ----------------------" << '\n';
 
       // Source Address
       std::cout << "Source Address: ";
@@ -151,7 +204,8 @@ namespace woXrooX{
     }
 
     static void outIPH(struct iphdr *iph){
-      std::cout << "IP Header" << '\n';
+      std::cout << "\n---------------------- outIPH ----------------------" << '\n';
+
 
       std::cout << "IP Version: " << (unsigned int)iph->version << '\n';
 
@@ -178,7 +232,31 @@ namespace woXrooX{
       std::cout << "Source IP: " << inet_ntoa(ip_source) << '\n';
       std::cout << "Destination IP: " << inet_ntoa(ip_destination) << '\n';
 
-      delete iph;
+      // delete iph;
+
+    }
+
+    static void outTCPH(unsigned char* inData){
+      struct iphdr *iph = (struct iphdr*)(inData + sizeof(struct ethhdr));
+      struct tcphdr *tcph = (struct tcphdr *)(inData + sizeof(struct ethhdr) + sizeof(struct iphdr));
+
+      std::cout << "\n---------------------- outTCPH ----------------------" << '\n';
+
+      std::cout << "TCP Header" << '\n';
+      std::cout << "Source Port: " << ntohs(tcph->source) << '\n';
+      std::cout << "Destination Port: " << ntohs(tcph->dest) << '\n';
+      std::cout << "Sequence Number: " << ntohl(tcph->seq) << '\n';
+      std::cout << "Acknowledge Number: " << ntohl(tcph->ack_seq) << '\n';
+      std::cout << "Header Length: " << tcph->doff*4 << " bytes" << '\n';
+      std::cout << "Window Size: " << ntohs(tcph->window) << '\n';
+      std::cout << "Checksum: " << ntohs(tcph->check) << '\n';
+      std::cout << "Urgent Pointer: " << tcph->urg_ptr << '\n';
+
+      // TCP Payload
+      int tcp_header_len = tcph->doff*4;
+      int payload_len = ntohs(iph->tot_len) - sizeof(struct iphdr) - tcp_header_len;
+      unsigned char* payload = inData + sizeof(struct ethhdr) + sizeof(struct iphdr) + tcp_header_len;
+      std::cout << "Payload: " << payload << '\n';
 
     }
 
@@ -191,6 +269,21 @@ namespace woXrooX{
       << "\nTCP: "      << Spotlight::TCP
       << "\nEGP: "      << Spotlight::EGP
       << "\nUDP: "      << Spotlight::UDP
+
+      // << "\nIPv4: "      << Spotlight::IPv4
+      // << "\nIPv6: "      << Spotlight::IPv6
+      // << "\nRAW: "      << Spotlight::RAW
+      // << "\nRDP: "      << Spotlight::RDP
+      // << "\nRTP: "      << Spotlight::RTP
+      // << "\nSSL: "      << Spotlight::SSL
+      // << "\nTLS: "      << Spotlight::TLS
+      // << "\nLLDP: "      << Spotlight::LLDP
+      // << "\nSTP: "      << Spotlight::STP
+      // << "\nARP: "      << Spotlight::ARP
+      // << "\nMAC_Security: "      << Spotlight::MAC_Security
+      // << "\nPTP: "      << Spotlight::PTP
+      // << "\nVLAN: "      << Spotlight::VLAN
+
       << "\nTotal: "    << Spotlight::total
       << "\nUnknown: "  << Spotlight::unknown
       << '\n';
@@ -236,6 +329,8 @@ namespace woXrooX{
     static int total, unknown;
     static int ICMP, IGMP, TCP, EGP, UDP;
 
+    static int IPv4, IPv6, RAW, RDP, RTP, SSL, TLS, LLDP, STP, ARP, MAC_Security, PTP, VLAN;
+
     static int fd_socket;
     static int bytes_received;
     static int BUFFER_SIZE;
@@ -250,6 +345,21 @@ namespace woXrooX{
   int Spotlight::TCP = 0;
   int Spotlight::EGP = 0;
   int Spotlight::UDP = 0;
+
+  // int Spotlight::IPv4 = 0;
+  // int Spotlight::IPv6 = 0;
+  // int Spotlight::RAW = 0;
+  // int Spotlight::RDP = 0;
+  // int Spotlight::RTP = 0;
+  // int Spotlight::SSL = 0;
+  // int Spotlight::TLS = 0;
+  // int Spotlight::LLDP = 0;
+  // int Spotlight::STP = 0;
+  // int Spotlight::ARP = 0;
+  // int Spotlight::MAC_Security = 0;
+  // int Spotlight::PTP = 0;
+  // int Spotlight::VLAN = 0;
+
 
   int Spotlight::fd_socket = -1;
   int Spotlight::bytes_received = 0;
